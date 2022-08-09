1724f4659c663d7ec8fd9976349417f43a0a8ffb
mehmet <tas349873@gmail.com>
2022-08-09 12:36:57 +0300

    update

--- src/componenets/Main.jsx
+++ src/componenets/Main.jsx
@@ -1,31 +1,47 @@
-import { useState } from "react";
+import axios from "axios";
+import React,{ useState } from "react";
 
 const Main = () => {
   const [searchText, setSearchText] = useState("");
-
+const [data, setData] = useState([])
   const handleChange = (e) => {
     setSearchText(e.target.value);
   };
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    getWeatherDataFromApi()
+    setSearchText("")
+  }
 const getWeatherDataFromApi = async()=>{
     let apiKey = process.env.REACT_APP_API_KEY;
     let units = "metric";
     let lang = "tr";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apiKey}&units=${units}&lang=${lang}`;
-}
+    try {
+        const response = await axios.get(url)
+        console.log(response);
+        const {main,name,sys,weather,id} = response.data;
+        const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+        setData([{main,name,sys,weather,iconUrl,id}])
+    } catch (err) {
+        console.log(err);
+    }
+};
   return (
     <section className="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           type="text"
           placeholder="Search for a city"
+          value={searchText}
           autoFocus
         />
         <button type="submit">SUBMIT</button>
         <span className="msg"></span>
       </form>
       <div className="container">
-        <ul className="cities">meta</ul>
+        <ul className="cities"></ul>
       </div>
     </section>
   );
