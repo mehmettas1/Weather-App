43c0155c68cb5b170daade3034a41e33778e8973
mehmet <tas349873@gmail.com>
2023-07-08 15:01:14 +0300

    update

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -14,3 +14,5 @@ const Footer = () => {
   
   export default Footer;
 
+
+
--- src/componenets/WeatherCard.jsx
+++ src/componenets/WeatherCard.jsx
@@ -1,21 +1,69 @@
-const WeatherCard = (props) => {
-  const { name, main, sys, iconUrl, weather } = props.data;
-  return (
-    <li className="city">
-      <h2 className="city-name">
-        <span>{name}</span>
-        <sup>{sys.country}</sup>
-      </h2>
-      <div className="city-temp">
-        {Math.round(main.temp)}
-        <sup>°C</sup>
-      </div>
-      <figure>
-        <img className="city-icon" src={iconUrl} alt="city-icon" />
-        <figcaption>{weather[0].description}</figcaption>
-      </figure>
-    </li>
-  );
-};
 
-export default WeatherCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const WeatherCard = (props) => {
+//   const { name, main, sys, iconUrl, weather } = props.data;
+//   return (
+//     <li className="city">
+//       <h2 className="city-name">
+//         <span>{name}</span>
+//         <sup>{sys.country}</sup>
+//       </h2>
+//       <div className="city-temp">
+//         {Math.round(main.temp)}
+//         <sup>°C</sup>
+//       </div>
+//       <figure>
+//         <img className="city-icon" src={iconUrl} alt="city-icon" />
+//         <figcaption>{weather[0].description}</figcaption>
+//       </figure>
+//     </li>
+//   );
+// };
+
+// export default WeatherCard;
