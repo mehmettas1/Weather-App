f6efac7b0976c0ebee05d2784ee7aa5713b3275e
mehmet <tas349873@gmail.com>
2023-07-10 14:41:04 +0300

    update

--- src/componenets/WeatherCard.jsx
+++ src/componenets/WeatherCard.jsx
@@ -1,23 +1,4 @@
 
-// import React from 'react'
-
-// const WeatherCard = (props) => {
-//   return (
-//     <li>
-//         <h2 className="city-name">
-//     <span>${name}</span>
-//     <sup>${sys.country}</sup>
-// </h2>
-// <div className="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-// <figure>
-//     <img className="city-icon" >
-//     <figcaption>${weather[0].description}</figcaption>
-// </figure>
-// </li>
-//   );
-// };
-
-// export default WeatherCard
 
 const WeatherCard = ({data}) => {
   const { name, main, sys, iconUrl, weather } = data;
